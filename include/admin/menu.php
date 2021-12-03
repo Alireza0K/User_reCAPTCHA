a684885123e6f5f6c189da24e09411b4201ba4b1
unknown <Alireza.karimi.new@gmail.com>
2021-12-03 20:24:20 +0330

    connect database to table

--- include/admin/menu.php
+++ include/admin/menu.php
@@ -38,16 +38,15 @@ function main_menu()
     global $wpdb;
 
     $Get_page = !isset($_GET["item"]) ? "1" : $_GET["item"];
-    $record_per_page = 3;
+    $record_per_page = 10;
     $get_data_for_pagination = $wpdb->get_results("Select * from {$wpdb->users}");
     $number_of_pagination =ceil($wpdb->num_rows/$record_per_page);
     
     if ($Get_page == $Get_page) {
         $get_page_to_int = intval($Get_page);
         $start_value = ($get_page_to_int - 1)*$record_per_page;
-        $pagination_complete = $wpdb->get_results("Select * from {$wpdb->prefix}certificate_information LIMIT ".$start_value.",".$record_per_page);
+        $pagination_complete = $wpdb->get_results("Select * from {$wpdb->users} LIMIT ".$start_value.",".$record_per_page);
     }
-    //get correct users 
     
     // add UI 
     $page_url_for_tab = $_GET["page"];
@@ -66,7 +65,7 @@ function first_submenu()
     if ($Get_page == $Get_page) {
         $get_page_to_int = intval($Get_page);
         $start_value = ($get_page_to_int - 1)*$record_per_page;
-        $pagination_complete = $wpdb->get_results("Select * from {$wpdb->prefix}certificate_information LIMIT ".$start_value.",".$record_per_page);
+        $pagination_complete = $wpdb->get_results("Select * from {$wpdb->users} LIMIT ".$start_value.",".$record_per_page);
     }
 
     // get badusers
