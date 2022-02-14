c9002b106b591345a686446d6d6d97eda53b3c53
Alireza0K <Alireza.karimi.programmer@gmail.com>
2022-02-14 18:00:41 +0330

    new version with debug

--- User_recaptcha_email.php
+++ User_recaptcha_email.php
@@ -4,7 +4,7 @@
  * Description: This plugin for check email and if email not success send a email to verify email 
  * Plugin URI: https://github.com/Alireza0K/User_reCAPTCHA
  * Author: Alireza0K
- * Version: 1.0.3
+ * Version: 1.0.4
  * Author URI: https://github.com/Alireza0K/
 */
 define("USER_RECAPTCH",plugin_dir_path(__FILE__)."include/");
